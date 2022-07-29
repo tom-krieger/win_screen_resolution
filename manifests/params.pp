a34ccf08bdf1841d412c11fb4a100609bc9aed65
Thomas Krieger <tom@tom-krieger.de>
2022-07-29 17:04:51 +0200

    fixed registry key path

--- manifests/params.pp
+++ manifests/params.pp
@@ -10,10 +10,10 @@
 class win_screen_resolution::params (
   Array $valid_screen_resolutions,
 ) {
-  $gems         = ['fiddle']
-  $script_dir   = 'C:\ProgramData\PuppetLabs\win_screen_resolution'
-  $script_file  = 'set_screen_resolution.ps1'
+  $gems          = ['fiddle']
+  $script_dir    = 'C:\ProgramData\PuppetLabs\win_screen_resolution'
+  $script_file   = 'set_screen_resolution.ps1'
   # $registry_key = 'HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Group Policy\Scripts\Logon\0\0\Script'
-  $registry_path = 'HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Group Policy\Scripts\Logon\0\0'
-  $registry_key = 'HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Group Policy\State\Machine\Scripts\Startup\0\0\Script'
+  $registry_path = 'HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Group Policy\State\Machine\Scripts\Startup\0\0'
+  $registry_key  = 'HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Group Policy\State\Machine\Scripts\Startup\0\0\Script'
 }

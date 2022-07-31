44ee198faf99a8790087afdcb83493299500e152
Thomas Krieger <tom@tom-krieger.de>
2022-07-31 17:03:33 +0200

    fixed script dir

--- manifests/params.pp
+++ manifests/params.pp
@@ -12,7 +12,17 @@ class win_screen_resolution::params (
 ) {
   $gems                 = ['fiddle']
   #$script_dir           = 'C:\ProgramData\PuppetLabs\win_screen_resolution'
-  $script_dir           = 'C:\ProgramData\PuppetLabs\win_screen_resolution'
+
+  if $facts['operatingsyetem'].downcase() == 'windows' {
+
+    $script_dir = $facts['operatingsystemmajrelease'] ? {
+      '2019'  => 'C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp',
+      default => 'C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp',
+    }
+  } else {
+    fail("OS ${facts['operatingsyetem']} not siupported!")
+  }
+
   $script_file          = 'set_screen_resolution.ps1'
   $state_registry_path  = 'HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Group Policy\State\Machine\Scripts'
   $policy_registry_path = 'HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Group Policy\Scripts'
